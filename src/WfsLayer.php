d7ee5a5685c66b06b931b6966b344074733601ae
ici.be <bruno.veyckemans@gmail.com>
2020-12-21 19:44:37 +0100

    Update WfsLayer.php

--- src/WfsLayer.php
+++ src/WfsLayer.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace ici\ici_tools;
 
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Component\DomCrawler\Crawler;
 
 class WfsLayer
 {
@@ -14,7 +16,14 @@ class WfsLayer
     private $sort_by;
     private $property_name;
     private $count;
+    private $result_type = 'results';
 
+    /**
+     * WfsLayer constructor.
+     * @param string $base_path
+     * @param string $layer_name
+     * @param string $version
+     */
     public function __construct(string $base_path, string $layer_name, string $version = '2.0.0')
     {
         $this->base_path = $base_path;
@@ -23,51 +32,14 @@ class WfsLayer
     }
 
     /**
+     * @param string $point
+     * @param int $meters
+     * @param string $geom_name
      * @return string
      */
-    public function getBasePath(): string
+    public static function filterByPointDistance(string $point, int $meters = 15, string $geom_name): string
     {
-        return $this->base_path;
-    }
-
-    /**
-     * @param string $base_path
-     */
-    public function setBasePath(string $base_path): void
-    {
-        $this->base_path = $base_path;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLayerName(): string
-    {
-        return $this->layer_name;
-    }
-
-    /**
-     * @param string $layer_name
-     */
-    public function setLayerName(string $layer_name): void
-    {
-        $this->layer_name = $layer_name;
-    }
-
-    /**
-     * @return string
-     */
-    public function getVersion(): string
-    {
-        return $this->version;
-    }
-
-    /**
-     * @param string $version
-     */
-    public function setVersion(string $version): void
-    {
-        $this->version = $version;
+        return "DWITHIN(".$geom_name.", ".$point.", ".$meters.", meters)";
     }
 
     /**
@@ -113,7 +85,7 @@ class WfsLayer
     /**
      * @return string
      */
-    public function getPropertyName()
+    public function getPropertyName(): string
     {
         return $this->property_name;
     }
@@ -142,6 +114,33 @@ class WfsLayer
         $this->count = $count;
     }
 
+    /**
+     * @return string
+     */
+    public function getQueryUrl(): string
+    {
+        return $this->getBasePath().'?'.http_build_query($this->getQueryFields());
+    }
+
+    /**
+     * @return string
+     */
+    public function getBasePath(): string
+    {
+        return $this->base_path;
+    }
+
+    /**
+     * @param string $base_path
+     */
+    public function setBasePath(string $base_path): void
+    {
+        $this->base_path = $base_path;
+    }
+
+    /**
+     * @return array
+     */
     public function getQueryFields(): array
     {
         $fields = array(
@@ -150,40 +149,136 @@ class WfsLayer
             'request' => 'GetFeature',
             'typeName' => $this->getLayerName(),
             'outputFormat' => 'json',
+            'resultType' => $this->result_type,
         );
 
-        if($this->output_srs) {
+        if ($this->output_srs) {
             $fields['srsName'] = 'EPSG:'.$this->output_srs;
         }
-        if($this->sort_by) {
+        if ($this->sort_by) {
             $fields['sortBy'] = $this->sort_by;
         }
-        if($this->sort_by) {
+        if ($this->property_name) {
             $fields['propertyname'] = $this->property_name;
         }
-        if($this->cql_filter) {
+        if ($this->cql_filter) {
             $fields['cql_filter'] = $this->cql_filter;
         }
-        if($this->count) {
-            $fields['count'] = $this->count;
+        if ($this->count) {
+            if ($this->getVersion()[0] > 1) {
+                $fields['count'] = $this->count;
+            } else {
+                $fields['maxFeatures'] = $this->count;
+            }
         }
 
         return $fields;
     }
 
-    public function getQueryUrl(): string
+    /**
+     * @return string
+     */
+    public function getVersion(): string
     {
-        return $this->getBasePath().'?'.http_build_query($this->getQueryFields());
+        return $this->version;
     }
 
-    public function getResults(): \stdClass
+    /**
+     * @param string $version
+     */
+    public function setVersion(string $version): void
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLayerName(): string
+    {
+        return $this->layer_name;
+    }
+
+    /**
+     * @param string $layer_name
+     */
+    public function setLayerName(string $layer_name): void
+    {
+        $this->layer_name = $layer_name;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getHits(): ?int
+    {
+        $this->setResultType('hits');
+        $httpClient = HttpClient::create();
+
+        try {
+            $response = $httpClient->request('POST', $this->getBasePath(), ['query' => $this->getQueryFields()]);
+            $statusCode = $response->getStatusCode();
+            if ($statusCode !== 200) {
+                return null;
+            }
+            $exp = explode('numberMatched="', $response->getContent());
+            $hits = strtok($exp[1], '"');
+
+        } catch (TransportExceptionInterface $e) {
+            return null;
+        }
+
+        return $hits;
+    }
+
+    /**
+     * @param string $result_type
+     */
+    public function setResultType(string $result_type): void
+    {
+        $this->result_type = $result_type;
+    }
+
+    /**
+     * @param bool $geometry
+     * @param bool $bbox
+     * @return array
+     */
+    public function getPropertiesArray(bool $geometry = false, bool $bbox = false): array
+    {
+        $this->setResultType('results');
+
+        $array = [];
+        $n = 0;
+        foreach ($this->getResults()->features as $feature) {
+            foreach ($feature->properties as $prop => $value) {
+                if ($bbox || $prop !== 'bbox') {
+                    $array[$n][$prop] = $value;
+                }
+            };
+            if ($geometry) {
+                $array[$n]['geometry'] = $feature->geometry;
+            }
+            $n++;
+        }
+
+        return $array;
+    }
+
+    /**
+     * @return \stdClass|null
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     */
+    public function getResults(): ?\stdClass
     {
         $httpClient = HttpClient::create();
 
         try {
             $response = $httpClient->request('POST', $this->getBasePath(), ['query' => $this->getQueryFields()]);
             $statusCode = $response->getStatusCode();
-            if($statusCode !== 200) {
+            if ($statusCode !== 200) {
                 return null;
             }
             $json = json_decode($response->getContent(), false);
@@ -193,12 +288,5 @@ class WfsLayer
 
         return $json;
     }
-
-    public static function filterByPointDistance(string $point, $meters = 15, string $geom_name): string
-    {
-       return "DWITHIN(".$geom_name.", ".$point.", ".$meters.", meters)";
-    }
-
-
-
+    
 }
